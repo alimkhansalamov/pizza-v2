f78c867ee262a83b1e90ffaa12cb076b235c31a6
Alimkhan8195 <alimkhan.salamov@gmail.com>
2022-07-18 00:59:39 +0200

    added search component for filtering

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Home from './pages/Home';
 import NotFound from './NotFound';
 import Cart from './pages/Cart';
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Search.module.scss';
+import Icon from './../../assets/img/search.svg';
+import Close from './../../assets/img/close.svg';
+const Search = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className={styles.root}>
+      <img className={styles.icon} src={Icon} alt="search icon" />
+      <input
+        value={searchValue}
+        className={styles.input}
+        placeholder="Search ..."
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
+      {searchValue && (
+        <img
+          onClick={() => setSearchValue('')}
+          className={styles.close}
+          src={Close}
+          alt="close icon"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Search;
